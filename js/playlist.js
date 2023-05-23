599fd46696f1b1242b401df835651b4d22d4db9d
Olmedev <olmedoalan821@gmail.com>
2023-05-23 19:03:00 -0300

    reproductor

---
+++ js/playlist.js
@@ -0,0 +1,35 @@
+let songs = [
+  {
+    name: "Give It Away",
+    artist: "Red Hot Chili Peppers",
+    img: "redhot",
+    audio: "Redhot"
+  },
+  {
+    name: "Mississipi Queen",
+    artist: "Mountain",
+    img: "Mountain",
+    audio: "Missisipi"
+  },
+  {
+    name: "Go With The Flow",
+    artist: "Queens Of The Stone Age",
+    img: "queenofthestone",
+    audio: "Queen"
+  },
+  {
+    name: "Stricken",
+    artist: "Disturbed",
+    img: "disturbed",
+    audio: "Disturbed"
+  },
+  {
+    name: "Master Of Puppets",
+    artist: "Metallica",
+    img: "masterofpuppets",
+    audio: "MasterOfPuppets"
+  },
+
+
+
+]
